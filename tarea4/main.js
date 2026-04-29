09f92edb548585b6fe70bf0ee2c770dd8eaa2e5f
tanatzu <tanatzu1@gmail.com>
2026-04-29 14:10:23 -0300

    ejercicio 1 hecho

---
+++ tarea4/main.js
@@ -0,0 +1,73 @@
+database = [{"id":1,"first_name":"Jeth","last_name":"Parmley","email":"jparmley0@t.co","gender":"Male","bank":"CITIZENS STATE BANK","city":"Caçapava","country":"Brazil","salary":"$99783.47","expenses":"$430.12"},
+{"id":2,"first_name":"Jane","last_name":"Clemmey","email":"jclemmey1@sogou.com","gender":"Female","bank":"ICCREA BANCA S.P.A. - ISTITUTO CENTRALE DEL CREDITO COOPERATIVO (IN FORMA ABBREVIATA: ICCREA BANCA S.P.A.)","city":"Belize City","country":"Belize","salary":"$46558.58","expenses":"$467.58"},
+{"id":3,"first_name":"Amy","last_name":"Slamaker","email":"aslamaker2@ihg.com","gender":"Female","bank":"CITIZENS STATE BANK","city":"Roioen","country":"Indonesia","salary":"$34899.91","expenses":"$869.32"},
+{"id":4,"first_name":"Garvin","last_name":"Whitehorn","email":"gwhitehorn3@craigslist.org","gender":"Genderfluid","bank":"PINNACLE BANK","city":"El Agua Dulcita","country":"Honduras","salary":"$47907.04","expenses":"$139.96"},
+{"id":5,"first_name":"Sholom","last_name":"Jefford","email":"sjefford4@free.fr","gender":"Male","bank":"FIRST SECURITY BANK","city":"Uppsala","country":"Sweden","salary":"$42577.92","expenses":"$492.03"},
+{"id":6,"first_name":"Tomasina","last_name":"Quiddihy","email":"tquiddihy5@columbia.edu","gender":"Female","bank":"FIRST NATIONAL BANK","city":"Guanshan","country":"China","salary":"$96212.18","expenses":"$185.74"},
+{"id":7,"first_name":"Stevy","last_name":"Manuaud","email":"smanuaud6@cmu.edu","gender":"Male","bank":"FIRST NATIONAL BANK","city":"Coronel Martínez","country":"Paraguay","salary":"$66080.30","expenses":"$800.91"},
+{"id":8,"first_name":"Kerrin","last_name":"Crosetto","email":"kcrosetto7@ucoz.ru","gender":"Genderqueer","bank":"BANCA DELLA MARCA CREDITO COOPERATIVO - SOCIETA' COOPERATIVA","city":"Liangshuihe","country":"China","salary":"$72688.36","expenses":"$354.29"},
+{"id":9,"first_name":"Neill","last_name":"Franzettoini","email":"nfranzettoini8@topsy.com","gender":"Male","bank":"GLACIER BANK","city":"Ladushkin","country":"Russia","salary":"$23289.94","expenses":"$780.05"},
+{"id":10,"first_name":"Phylys","last_name":"Goodsall","email":"pgoodsall9@umich.edu","gender":"Female","bank":"PEOPLES BANK","city":"Buan","country":"Philippines","salary":"$98237.29","expenses":"$424.75"},
+{"id":11,"first_name":"Hendrick","last_name":"Bullough","email":"hbullougha@forbes.com","gender":"Male","bank":"Abanca Corporacion Bancaria, S.A.","city":"Đắk Glei","country":"Vietnam","salary":"$69675.02","expenses":"$515.45"},
+{"id":12,"first_name":"Minta","last_name":"Brimley","email":"mbrimleyb@xrea.com","gender":"Female","bank":"CORNERSTONE BANK","city":"Szczawno-Zdrój","country":"Poland","salary":"$52793.58","expenses":"$812.49"},
+{"id":13,"first_name":"Rooney","last_name":"de Clerk","email":"rdeclerkc@dropbox.com","gender":"Male","bank":"BANK OF AMERICA","city":"Seropédica","country":"Brazil","salary":"$15784.98","expenses":"$686.92"},
+{"id":14,"first_name":"Jakie","last_name":"Ismail","email":"jismaild@canalblog.com","gender":"Male","bank":"BANCO BPM SOCIETA' PER AZIONI","city":"Penedo","country":"Portugal","salary":"$83006.46","expenses":"$840.14"},
+{"id":15,"first_name":"Danie","last_name":"Blackstock","email":"dblackstocke@yahoo.co.jp","gender":"Male","bank":"BANK OF AMERICA","city":"Urambo","country":"Tanzania","salary":"$82594.78","expenses":"$234.50"},
+{"id":16,"first_name":"Ermanno","last_name":"Stienham","email":"estienhamf@msn.com","gender":"Male","bank":"BANK OF AMERICA","city":"Belfast","country":"Canada","salary":"$43449.13","expenses":"$113.01"},
+{"id":17,"first_name":"Kalli","last_name":"Laming","email":"klamingg@hugedomains.com","gender":"Female","bank":"Goyer & Göppel KG","city":"Stetseva","country":"Ukraine","salary":"$13141.12","expenses":"$809.70"},
+{"id":18,"first_name":"Ernesta","last_name":"Whicher","email":"ewhicherh@boston.com","gender":"Female","bank":"Volksbank Göppingen eG","city":"Cincinnati","country":"United States","salary":"$71991.20","expenses":"$413.80"},
+{"id":19,"first_name":"Bartolomeo","last_name":"Sturm","email":"bsturmi@odnoklassniki.ru","gender":"Male","bank":"Banco Pichincha España, S.A..","city":"Chutove","country":"Ukraine","salary":"$70471.66","expenses":"$786.06"},
+{"id":20,"first_name":"Patrica","last_name":"Hissie","email":"phissiej@mayoclinic.com","gender":"Female","bank":"FIRST FINANCIAL BANK","city":"Zhutian","country":"China","salary":"$27564.44","expenses":"$110.06"},
+{"id":21,"first_name":"Ogdan","last_name":"Brade","email":"obradek@wufoo.com","gender":"Male","bank":"BANCA DI CREDITO COOPERATIVO DI RECANATI E COLMURANO - SOCIETA' COOPERATIVA","city":"Liski","country":"Russia","salary":"$99783.72","expenses":"$219.36"},
+{"id":22,"first_name":"Ruby","last_name":"Tyler","email":"rtylerl@mail.ru","gender":"Female","bank":"INDEPENDENT BANK","city":"Puteaux","country":"France","salary":"$68716.16","expenses":"$894.35"},
+{"id":23,"first_name":"Templeton","last_name":"Liebrecht","email":"tliebrechtm@jimdo.com","gender":"Male","bank":"Volksbank eG","city":"Sabhā","country":"Libya","salary":"$48950.53","expenses":"$537.96"},
+{"id":24,"first_name":"Shannen","last_name":"Cardus","email":"scardusn@ftc.gov","gender":"Female","bank":"Raiffeisenbank Timelkam-Lenzing-Puchkirchen eGen","city":"Gambiran","country":"Indonesia","salary":"$45338.85","expenses":"$572.73"},
+{"id":25,"first_name":"Weylin","last_name":"Highwood","email":"whighwoodo@yale.edu","gender":"Male","bank":"PINNACLE BANK","city":"Cincinnati","country":"United States","salary":"$31876.73","expenses":"$880.43"},
+{"id":26,"first_name":"Madlin","last_name":"Bantham","email":"mbanthamp@list-manage.com","gender":"Female","bank":"NOBA Bank Group AB (publ)","city":"Lagoa do Itaenga","country":"Brazil","salary":"$83025.07","expenses":"$686.56"},
+{"id":27,"first_name":"Evin","last_name":"Muzzollo","email":"emuzzolloq@sbwire.com","gender":"Agender","bank":"USALLIANCE FEDERAL CREDIT UNION","city":"Benito Juarez","country":"Mexico","salary":"$37406.59","expenses":"$610.08"},
+{"id":28,"first_name":"Aviva","last_name":"Goodhay","email":"agoodhayr@disqus.com","gender":"Female","bank":"RENASANT BANK","city":"Lawrenceville","country":"United States","salary":"$80163.28","expenses":"$144.51"},
+{"id":29,"first_name":"Kitti","last_name":"Wilmington","email":"kwilmingtons@trellian.com","gender":"Non-binary","bank":"“Swedbank”, AB","city":"Scranton","country":"United States","salary":"$97551.20","expenses":"$945.50"},
+{"id":30,"first_name":"Ezra","last_name":"Dickman","email":"edickmant@networksolutions.com","gender":"Male","bank":"FIRSTBANK","city":"Willowdale","country":"Canada","salary":"$75218.90","expenses":"$853.82"},
+{"id":31,"first_name":"Eachelle","last_name":"Platts","email":"eplattsu@prlog.org","gender":"Female","bank":"VALLEY NATIONAL BANK","city":"Nuamuzi","country":"Indonesia","salary":"$50845.84","expenses":"$739.61"},
+{"id":32,"first_name":"Merrie","last_name":"Broomfield","email":"mbroomfieldv@webmd.com","gender":"Female","bank":"PINNACLE BANK","city":"Tongxing","country":"China","salary":"$33686.77","expenses":"$635.27"},
+{"id":33,"first_name":"Erda","last_name":"Florence","email":"eflorencew@skyrock.com","gender":"Agender","bank":"PSD Bank Rhein-Ruhr eG","city":"Lais","country":"Philippines","salary":"$82456.00","expenses":"$478.76"},
+{"id":34,"first_name":"Elsie","last_name":"Casarini","email":"ecasarinix@noaa.gov","gender":"Female","bank":"WSK Bank AG","city":"Xinsheng","country":"China","salary":"$40258.09","expenses":"$434.76"},
+{"id":35,"first_name":"Desiri","last_name":"Peek","email":"dpeeky@free.fr","gender":"Female","bank":"FIRST NATIONAL BANK","city":"Roissy Charles-de-Gaulle","country":"France","salary":"$77358.69","expenses":"$687.91"},
+{"id":36,"first_name":"Sybila","last_name":"Clavey","email":"sclaveyz@histats.com","gender":"Female","bank":"CASSA RURALE VALLAGARINA - BANCA DI CREDITO COOPERATIVO - SOCIETA' COOPERATIVA","city":"Manikchari","country":"Bangladesh","salary":"$29801.36","expenses":"$588.94"},
+{"id":37,"first_name":"Morgan","last_name":"Beatens","email":"mbeatens10@upenn.edu","gender":"Female","bank":"Athenry Credit Union Limited","city":"Midlands","country":"Mauritius","salary":"$40458.51","expenses":"$487.30"},
+{"id":38,"first_name":"Renaldo","last_name":"Yesichev","email":"ryesichev11@g.co","gender":"Male","bank":"FIRST NATIONAL BANK OF PENNSYLVANIA","city":"Sidamukti","country":"Indonesia","salary":"$23640.33","expenses":"$703.30"},
+{"id":39,"first_name":"Dougie","last_name":"De Stoop","email":"ddestoop12@digg.com","gender":"Male","bank":"BEAR STATE BANK","city":"São Miguel","country":"Portugal","salary":"$26218.37","expenses":"$714.17"},
+{"id":40,"first_name":"Hynda","last_name":"Gerrelts","email":"hgerrelts13@netlog.com","gender":"Female","bank":"CITIZENS NATIONAL BANK","city":"Outeirô","country":"Portugal","salary":"$44545.65","expenses":"$220.56"},
+{"id":41,"first_name":"Desdemona","last_name":"Goodfellow","email":"dgoodfellow14@pcworld.com","gender":"Female","bank":"Raiffeisenbank Höchberg eG","city":"Macau","country":"Brazil","salary":"$97974.87","expenses":"$409.99"},
+{"id":42,"first_name":"Lona","last_name":"Cuxon","email":"lcuxon15@skyrock.com","gender":"Female","bank":"The Bank of New York Mellon SA/NV, Amsterdam Branch","city":"Jamālpur","country":"Bangladesh","salary":"$14865.35","expenses":"$351.03"},
+{"id":43,"first_name":"Marybelle","last_name":"Conquest","email":"mconquest16@shareasale.com","gender":"Genderfluid","bank":"ING Bank N.V.","city":"Kraczkowa","country":"Poland","salary":"$53179.68","expenses":"$371.79"},
+{"id":44,"first_name":"Abelard","last_name":"Towns","email":"atowns17@yelp.com","gender":"Male","bank":"AKBANK T.A.S. Malta Branch","city":"Santo Domingo","country":"Philippines","salary":"$30898.49","expenses":"$524.30"},
+{"id":45,"first_name":"Maje","last_name":"Penkethman","email":"mpenkethman18@storify.com","gender":"Male","bank":"BMO HARRIS BANK","city":"Isiolo","country":"Kenya","salary":"$76124.32","expenses":"$888.29"},
+{"id":46,"first_name":"Caty","last_name":"Gepheart","email":"cgepheart19@opera.com","gender":"Female","bank":"Sparkasse Neu-Ulm-Illertissen","city":"Bissorã","country":"Guinea-Bissau","salary":"$43237.29","expenses":"$601.63"},
+{"id":47,"first_name":"Natasha","last_name":"Pasterfield","email":"npasterfield1a@discovery.com","gender":"Genderqueer","bank":"FIMBank plc","city":"Dalongtan","country":"China","salary":"$35903.47","expenses":"$220.67"},
+{"id":48,"first_name":"Sandye","last_name":"Longland","email":"slongland1b@china.com.cn","gender":"Female","bank":"HUNTINGTON NATIONAL BANK","city":"Dorp Antriol","country":"Bonaire, Saint Eustatius and Saba ","salary":"$49085.97","expenses":"$147.21"},
+{"id":49,"first_name":"Nigel","last_name":"Jeffers","email":"njeffers1c@uol.com.br","gender":"Male","bank":"BMO HARRIS BANK","city":"Eustaquio Lopez","country":"Philippines","salary":"$58386.20","expenses":"$605.04"},
+{"id":50,"first_name":"Rivi","last_name":"Bordone","email":"rbordone1d@geocities.jp","gender":"Agender","bank":"Lombard Bank Malta plc","city":"Fucheng","country":"China","salary":"$99682.90","expenses":"$837.97"}]
+
+
+// ACTIVIDAD 1:
+id_ingresado = Number(prompt("ingrese ID:"))
+
+function obtener_datos(){
+    database.forEach(i => {
+        salario = i["salary"].replace("$", "")
+        expensas = i["expenses"].replace("$", "")
+
+        if (i["id"] === id_ingresado){
+            console.log(`nombre completo: ${i["first_name"]} ${i["last_name"]}`)
+            console.log(`banco: ${i["bank"]}`)
+            console.log(`ahorro mensual: ${salario - expensas}`)
+        }
+    })
+}
+
+obtener_datos();
+
+
+
+// ACTIVIDAD 2:
